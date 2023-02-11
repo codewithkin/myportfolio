f20ec479fd90f58d3f857d688e9b797dc6088260
zinzombe_kin <kinzinzombe07@gmail.com>
2023-02-11 20:59:13 +0200

    Configured nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,69 @@ import Contact from './pages/Contact'
 import Experience from './pages/Experience'
 import Home from './pages/Home'
 import Services from './pages/Services'
-import { motion } from 'framer-motion'
+import { animate, motion } from 'framer-motion'
+import { BiMenu } from 'react-icons/bi';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showNav, setShowNav] = useState(false);
+  const showNavBar = () => {
+    setShowNav(true)
+  }
 
   return (
     <section className="App">
-      <section className="container">
-        <motion.h1 animate={{ fontSize: '1.75rem' }} className=' text-center font-bold text-gray-800'>Kin Zinzombe</motion.h1>
+      <section className="container flex gap-2">
+        <BiMenu className={ showNav ? 'hidden' : 'w-[2rem] h-[2rem] self-center block md:hidden' } onClick={() => showNavBar}/>
+        <motion.h1 animate={{ fontSize: '1.75rem' }} className=' font-bold text-gray-800'>Kin Zinzombe</motion.h1>
       </section>
+
+      <header className="bg-black grid p-6 text-white">
+                <article className="text-center mb-2">
+                    <p className="text-xl">
+                    I'm Kin, I am a full stack developer specializing in the MERN stack. I use javascript frameworks and
+                    libraries such as React, Express, and Framer motion
+                    to create beautiful and lightning fast website frontends as well as robust RESTful API backends.
+                    </p>
+                </article>
+
+                <motion.article whileHover={{  x:-20, y:-10 }} className="bg-white flex justify-center align-middle rounded-xl p-2 justify-self-center">
+                    <img src='./src/assets/image.jpg' alt="developer" className="w-[12rem] h-[12rem]"/>
+                </motion.article>
+            </header>
+      <article>
+                <motion.ul className=" hidden desktop-links md:block">
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/services'>Services</Link>
+                    </li>
+                    <li>
+                        <Link to='/experience'>Experience</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>Contact</Link>
+                    </li>
+                </motion.ul>
+      </article>
+
+      <article >
+                <motion.ul className={ showNav ? 'block' : "hidden links" }>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/services'>Services</Link>
+                    </li>
+                    <li>
+                        <Link to='/experience'>Experience</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>Contact</Link>
+                    </li>
+                </motion.ul>
+      </article>
+
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/services' element={<Services/>} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,19 +3,7 @@ import { motion } from "framer-motion";
 const Home = () => {
     return ( 
         <section>
-            <header className="bg-black grid p-6 text-white">
-                <article className="text-center mb-2">
-                    <p className="text-xl">
-                    I'm Kin, I am a full stack developer specializing in the MERN stack. I use javascript frameworks and
-                    libraries such as React, Express, and Framer motion
-                    to create beautiful and lightning fast website frontends as well as robust RESTful API backends.
-                    </p>
-                </article>
-
-                <motion.article whileHover={{  x:-20, y:-10 }} className="bg-white flex justify-center align-middle rounded-xl p-2 justify-self-center">
-                    <img src='./src/assets/image.jpg' alt="developer" className="w-[12rem] h-[12rem]"/>
-                </motion.article>
-            </header>
+<h1>Home</h1>
         </section>
      );
 }
