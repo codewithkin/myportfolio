fdc249631f351441e9f3caf13fa1602ee3bad86b
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2023-02-11 18:11:14 +0200

    Added header and animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
 import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import Contact from './pages/Contact'
 import Experience from './pages/Experience'
 import Home from './pages/Home'
 import Services from './pages/Services'
+import { motion } from 'framer-motion'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <section className="App">
+      <section className="container">
+        <motion.h1 animate={{ fontSize: '1.75rem' }} className=' text-center font-bold text-gray-800'>Kin Zinzombe</motion.h1>
+      </section>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home/>} />
         <Route path='/services' element={<Services/>} />
-        <Route path='/experience' element={<Experience/>}/>
+        <Route path='/experience' element={<Experience/>} />
         <Route path='/contact' element={<Contact/>} />
       </Routes>
-
-      <section className="container">
-        <nav>
-          <h1>Kin Zinzombe</h1>
-        </nav>
-      </section>
     </section>
   )
 }
