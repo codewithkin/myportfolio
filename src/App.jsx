1ff2d8d9f122d3bd5401282848621acb89d48fd2
zinzombe_kin <kinzinzombe07@gmail.com>
2023-02-12 13:46:39 +0200

    Added NavBar styles for desktop

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ import { BiMenu } from 'react-icons/bi';
 import { AiOutlineClose } from 'react-icons/ai'
 
 function App() {
+  const [show, setShow] = useState('more');
   const [showNav, setShowNav] = useState(false);
+  const [showMore, setShowMore] = useState(false);
   const showNavBar = () => {
     if(showNav == false) setShowNav(true)
     else {
@@ -18,46 +20,65 @@ function App() {
     }
   }
 
+  const handleShowMore = () => {
+    if(!showMore) { 
+      setShowMore(true)
+      setShow('less')
+    } else {
+      setShowMore(false)
+      setShow('more')
+    }
+  }
+
   return (
-    <main className="App relative h-full">
-      <nav className="flex">
+    <main className="App h-full">
+      <nav className="flex ">
         <section className="container flex gap-2">
         <BiMenu className={ showNav ? 'hidden' : 'w-[2rem] h-[2rem] self-center block md:hidden' } onClick={showNavBar}/>
           <motion.h1 animate={{ fontSize: '1.75rem' }} className=' font-bold text-gray-800 ml-4'>Kin Zinzombe</motion.h1>
         </section>
 
-        <article className="mobile-links self-center">
+        <article className="mobile-links hidden md:block self-center">
           <ul className='flex gap-4 text-xl mr-4'>
             <li>
-              <Link to='/'>Home</Link>
+              <Link to='/' className='text-gray-700 hover:text-gray-400'>Home</Link>
             </li>
             <li>
-              <Link to='services'>Services</Link>
+              <Link to='services' className='text-gray-700 hover:text-gray-400'>Services</Link>
             </li>
             <li>
-              <Link to='/exeperience'> Experience</Link>
+              <Link to='/exeperience' className='text-gray-700 hover:text-gray-400'> Experience</Link>
             </li>
             <li>
-              <Link to='/contact'>Contact</Link>
+              <Link to='/contact' className='text-gray-700 hover:text-gray-400'>Contact</Link>
             </li>
           </ul>
         </article>
       </nav>
 
-        <header className="bg-black grid p-6 text-white">
-                <article className="text-center mb-2">
+        <header className="bg-black relative md:flex md:justify-between grid p-6 text-white">
+                <article className="text-center grid mb-2 md:w-[50rem] p-5">
                     <p className="text-xl">
                     I'm Kin, I am a full stack developer specializing in the MERN stack. I use javascript frameworks and
                     libraries such as React, Express, and Framer motion
                     to create beautiful and lightning fast website frontends as well as robust RESTful API backends.
+                    <span 
+                    style={{ transition: 'display' }}
+                    className={ showMore ? 'block md:block' : 'hidden md:block' }
+                    >
+                      I started learning programming in March of 2020, I learnt HTML, CSS and JavaScript for a year. I've 
+                      Only just recenty started to learn JavaScript Frameworks such as the Express framework on the NodeJS runtime and JavaScipt libraries such 
+                      as the ReactJS library. I also use the TailwindCSS framework.
+                    </span>
+                    <button className='block md:hidden justify-self-center self-center' onClick={handleShowMore}>Show { show }</button>
                     </p>
                 </article>
 
-                <motion.article whileHover={{  x:-20, y:-10 }} className="bg-white flex flex-col justify-center align-middle rounded-xl justify-self-center">
-                    <section><img src='./src/assets/image.jpg' alt="developer" className="w-full h-[20rem] rounded-tl-xl rounded-tr-xl"/></section>
+                <motion.article whileHover={{  x:-20, y:-10 }} className= " bg-white w-full h-full relative md:w-[30rem] flex flex-col justify-center align-middle rounded-xl justify-self-center">
+                    <section><img src='./src/assets/image.jpg' alt="developer" className="w-full md:h-[15rem] rounded-tl-xl rounded-tr-xl"/></section>
 
                     <section className=''>
-                      <p className='text-black font-bold text-center p-2'>
+                      <p className='text-black font-bold text-center p-4'>
                         When I'm not busy reversing a string or connecting a client's website to a databse I enjoy <span className='text-red-800'>writing</span>,
                         <span className='text-red-800'>reading</span> and <span className='text-red-800'> exercising.</span> I write poetry, mystery books, 
                         and blog entries to share my coding experiences with other programmers and to track my coding career.
@@ -65,22 +86,24 @@ function App() {
                     </section>
                 </motion.article>
         </header>
-      <article>
-                <motion.ul className=" hidden desktop-links md:block">
-                    <li>
-                        <Link to='/'>Home</Link>
+
+        <article className={ showNav ? 'block absolute top-0 left-0 h-[48rem]  w-3/4 bg-black' : 'hidden' }>
+                  <AiOutlineClose fill='white' onClick={ showNavBar } size='28' className='mt-4 ml-4'/>
+                  <ul className='text-2xl  grid gap-20'>
+                    <li className='ml-5 mt-4'>
+                      <Link to='/' className='underline text-gray-700 hover:text-gray-200'>Home</Link>
                     </li>
-                    <li>
-                        <Link to='/services'>Services</Link>
+                    <li className='ml-5'>
+                      <Link to='services' className='underline text-gray-700 hover:text-gray-200 '>Services</Link>
                     </li>
-                    <li>
-                        <Link to='/experience'>Experience</Link>
+                    <li className='ml-5'>
+                      <Link to='/exeperience' className='underline text-gray-700 hover:text-gray-200 '> Experience</Link>
                     </li>
-                    <li>
-                        <Link to='/contact'>Contact</Link>
+                    <li className='ml-5'>
+                      <Link to='/contact' className='underline text-gray-700 hover:text-gray-200'>Contact</Link>
                     </li>
-                </motion.ul>
-      </article>
+                  </ul>
+                </article>
 
       <Routes>
         <Route path='/' element={<Home/>} />
